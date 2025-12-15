6f6dac686429ea955820b52b5f9c3731fd21f2fa
kartofelr <johnmepno@gmail.com>
2025-12-15 11:53:05 +0100

    added sdl_test

--- src/Engine/GameWindow.cpp
+++ src/Engine/GameWindow.cpp
@@ -1,20 +1,38 @@
 #include "Engine/GameWindow.hpp"
+#include <cstdio>
 #include <SDL2/SDL.h>
 
-bool GameWindow::initialize() {
-  if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-    return false;
-  }
+void sdl_test () {
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        std::fprintf(stderr, "SDL_Init Error: %s\n", SDL_GetError());
+    }
 
-  SDL_Window *window =
-      SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-                       width, height, SDL_WINDOW_SHOWN + SDL_WINDOW_FULLSCREEN);
-  if (!window) {
+    SDL_Window* window = SDL_CreateWindow(
+        "SDL Hello World",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        800,
+        600,
+        SDL_WINDOW_SHOWN
+    );
+
+    if (!window) {
+        std::fprintf(stderr, "SDL_CreateWindow Error: %s\n", SDL_GetError());
+        SDL_Quit();
+    }
+
+    bool running = true;
+    SDL_Event event;
+
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = false;
+            }
+        }
+        SDL_Delay(16); // ~60 FPS idle
+    }
+
+    SDL_DestroyWindow(window);
     SDL_Quit();
-    return false;
-  }
-
-  return true;
-}
-
-void GameWindow::kill() { SDL_Quit(); }
+};
