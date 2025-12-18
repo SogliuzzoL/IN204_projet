67e55b8d6a13f8900a03401587de3de496798650
Loris SOGLIUZZO <loris@sogliuzzo.com>
2025-12-18 15:52:46 +0100

    Refactor server function and improve main function structure for clarity

--- src/main.cpp
+++ src/main.cpp
@@ -4,46 +4,42 @@
 #include "Network/NetworkClient.hpp"
 #include "Network/NetworkServer.hpp"
 
-int main(int argc, char** argv) {
-  // Server Mode
-  if (argc > 1) {
-    if (std::string(argv[1]) == "--server") {
-      std::cout << "Starting in server mode..." << std::endl;
+int server() {
+  std::cout << "Starting in server mode..." << std::endl;
 
-      NetworkServer server;
+  NetworkServer server;
 
-      // Initialize network
-      if (!server.initialize()) {
-        std::cerr << "Failed to initialize network." << std::endl;
-        return -1;
-      }
-
-      // Start server
-      if (!server.start("0.0.0.0", 12345)) {
-        std::cerr << "Failed to start server." << std::endl;
-        server.quit();
-        return -1;
-      }
-
-      std::cout << "Server started successfully. Waiting for connections..."
-                << std::endl;
-
-      while (1) {
-        server.handleNewConnections();
-      }
-
-      // Stop server
-      server.stop();
-      // Quit network
-      server.quit();
-
-      std::cout << "Server shut down successfully." << std::endl;
-
-      return 0;
-    }
+  // Initialize network
+  if (!server.initialize()) {
+    std::cerr << "Failed to initialize network." << std::endl;
+    return -1;
   }
 
-  // Client Mode
+  // Start server
+  if (!server.start("0.0.0.0", 12345)) {
+    std::cerr << "Failed to start server." << std::endl;
+    server.quit();
+    return -1;
+  }
+
+  std::cout << "Server started successfully. Waiting for connections..."
+            << std::endl;
+
+  while (1) {
+    server.handleNewConnections();
+  }
+
+  // Stop server
+  server.stop();
+  // Quit network
+  server.quit();
+
+  std::cout << "Server shut down successfully." << std::endl;
+
+  return 0;
+}
+
+int client() {
   std::cout << "Starting in client mode..." << std::endl;
 
   NetworkClient client;
@@ -65,4 +61,13 @@ int main(int argc, char** argv) {
   std::cout << "Client shut down successfully." << std::endl;
 
   return 0;
+}
+
+int main(int argc, char** argv) {
+  // Server Mode
+  if (argc > 1)
+    if (std::string(argv[1]) == "--server") return server();
+
+  // Client Mode
+  return client();
 }
